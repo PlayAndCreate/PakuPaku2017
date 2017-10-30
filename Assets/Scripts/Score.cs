08b9229797b22db17a1369f522d5537af1aefe72
git_haya <hw15a186@oecu.jp>
2017-10-30 19:21:40 +0900

    10/30(水上)
    【変更点】
    イラスト素材をを追加しました。

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//**********開始**********//
+using UnityEngine.UI;
+//**********終了**********//
+
+public class Score : MonoBehaviour {
+
+    //**********開始**********//
+    public Text scoreText;
+    private int score = 0;
+    //**********終了**********//
+
+        
+
+// Use this for initialization
+void Start () {
+        scoreText.text = "Score : 0";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.M))
+        {
+            //スコアに加算
+            score += 10;
+
+            //スコアを更新
+            scoreText.text = "Score : " + score.ToString();
+        }
+    }
+}
